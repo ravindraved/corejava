b1bcbcbf342e3cc64a28bd44f210c80c9256ad6b
ravindra <rved76@gmail.com>
2018-06-25 09:34:14 +0530

    check2words..skel

---
+++ j2se/src/main/java/in/rv/study/ssa/Check2WordsCharMatch.java
@@ -0,0 +1,26 @@
+package in.rv.study.ssa;
+
+import java.util.Scanner;
+
+/*
+ * ssa -> Simple School Assignments
+ * 
+ * Given two words check if 2nd world contains exactly same characters that are in word1
+ * 
+ */
+public class Check2WordsCharMatch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner console = new Scanner(System.in);
+		System.out.println("Type Word 1");
+		String strWord1 = console.nextLine();
+
+		System.out.println("Type Word 2");
+		String strWord2 = console.nextLine();
+
+		
+		
+	}
+
+}
