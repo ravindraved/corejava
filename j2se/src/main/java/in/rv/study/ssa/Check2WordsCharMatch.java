1f8e0961c134cb182a351779d682b6b5227e71bf
ravindra <rved76@gmail.com>
2018-06-25 23:19:31 +0530

    udated maven jdk.version implemented matching code

--- j2se/src/main/java/in/rv/study/ssa/Check2WordsCharMatch.java
+++ j2se/src/main/java/in/rv/study/ssa/Check2WordsCharMatch.java
@@ -1,26 +1,57 @@
 package in.rv.study.ssa;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
  * ssa -> Simple School Assignments
- * 
+ *
  * Given two words check if 2nd world contains exactly same characters that are in word1
- * 
+ *
  */
 public class Check2WordsCharMatch {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner console = new Scanner(System.in);
-		System.out.println("Type Word 1");
-		String strWord1 = console.nextLine();
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner console = new Scanner(System.in);
+        System.out.println("Type Word 1");
+        String strWord1 = console.nextLine();
 
-		System.out.println("Type Word 2");
-		String strWord2 = console.nextLine();
+        System.out.println("Type Word 2");
+        String strWord2 = console.nextLine();
 
-		
-		
-	}
+        if (
+                (strWord1.length() == strWord2.length()) &&
+                        (strWord1.length() != 0)
+
+                ) {
+
+            char[] charArray1 = strWord1.toUpperCase().toCharArray();
+            Arrays.sort(charArray1);
+
+            char[] charArray2 = strWord2.toUpperCase().toCharArray();
+            Arrays.sort(charArray2);
+
+            if (
+
+                    (Arrays.toString(charArray1)).equals(
+                            (Arrays.toString(charArray2))
+
+                    )
+
+                    ) {
+
+                System.out.println("Matches!!");
+
+
+            } else {
+                System.out.println("No Match!!");
+            }
+
+        } else {
+            System.out.println("No Match!!");
+        }
+
+    }
 
 }
